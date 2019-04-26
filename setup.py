7bddf7a66307f4e0f7aa5caee4347658dd7605c7
Cologler <skyoflw@gmail.com>
2019-04-27 01:34:59 +0800

    update env

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from setupmeta_builder import setup_it
+
+setup_it()
